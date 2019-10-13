53cb822609321393214e526881aa96167ddb463d
skibitsky <15127974+skibitsky@users.noreply.github.com>
2019-10-13 20:21:51 +0300

    Project Upload

---
+++ Assets/unity-now/Scripts/ConfigureNow.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace com.skibitsky.UnityNow
+{
+    /// <summary>
+    /// Configuration Scriptable Object. Must be located at Assets/unity-now/ConfigureNow
+    /// </summary>
+    // Uncomment the line bellow if you lost the Scriptable Object and need to create a new one.
+    // [CreateAssetMenu(fileName = "ConfigureNow", menuName = "unity-now/ConfigureNow", order = 0)]
+    public class ConfigureNow : ScriptableObject
+    {
+        [SerializeField] public string Token;
+
+        [SerializeField] public string BaseUrl = "https://api.zeit.co/v9/now";
+
+        [SerializeField] public bool CopyUrl = true;
+    }
+}
